943b817d7bf853cca1b8b717bbe115e48c59bda2
Juan Almeyda <almeydajuan@gmail.com>
2016-07-04 16:27:58 -0300

    added webhook

---
+++ src/main/java/ar/com/jalmeyda/magnetbot/controller/FacebookIntegrationController.java
@@ -0,0 +1,26 @@
+package ar.com.jalmeyda.magnetbot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Juan Almeyda on 7/4/2016.
+ */
+@RestController
+public class FacebookIntegrationController {
+
+    private static final String VALIDATION_TOKEN = "EAANeGQLckDoBAAQxsufSXijxEvZChK4iVrFGX4zL3VJCFctFb9mIUhC4DyEcm0vQ5qM1sK04p4ZAvZC9QK5zvqHc7HFpiGb4RzkEZBZA8skWKZB123ZCvIOa0tvMBaUuLOIsy1zvOtqQfZCSFSrq51Ir3ZCSb7niRFKirZBs2M1UtVQgZDZD";
+
+    @RequestMapping("/webhook")
+    public String webHook(@RequestParam(name = "hub.mode") String mode,
+                          @RequestParam(name = "hub.verify_token") String token,
+                          @RequestParam(name = "hub.challenge") String challenge) {
+        System.out.println("********************************************************************");
+        System.out.println(mode);
+        System.out.println(token);
+        System.out.println(challenge);
+        System.out.println("********************************************************************");
+        return challenge;
+    }
+}
