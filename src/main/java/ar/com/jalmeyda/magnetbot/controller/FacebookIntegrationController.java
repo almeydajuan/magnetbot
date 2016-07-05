a9fb8b92a65dfbf7238da304f9b08032043710e7
Juan Almeyda <almeydajuan@gmail.com>
2016-07-05 12:13:29 -0300

    added logback

--- src/main/java/ar/com/jalmeyda/magnetbot/controller/FacebookIntegrationController.java
+++ src/main/java/ar/com/jalmeyda/magnetbot/controller/FacebookIntegrationController.java
@@ -1,5 +1,7 @@
 package ar.com.jalmeyda.magnetbot.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,16 +13,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class FacebookIntegrationController {
 
     private static final String VALIDATION_TOKEN = "EAANeGQLckDoBAAQxsufSXijxEvZChK4iVrFGX4zL3VJCFctFb9mIUhC4DyEcm0vQ5qM1sK04p4ZAvZC9QK5zvqHc7HFpiGb4RzkEZBZA8skWKZB123ZCvIOa0tvMBaUuLOIsy1zvOtqQfZCSFSrq51Ir3ZCSb7niRFKirZBs2M1UtVQgZDZD";
+    private Logger logger = LoggerFactory.getLogger(FacebookIntegrationController.class);
+
 
     @RequestMapping("/webhook")
     public String webHook(@RequestParam(name = "hub.mode", required = false) String mode,
                           @RequestParam(name = "hub.verify_token", required = false) String token,
                           @RequestParam(name = "hub.challenge", required = false, defaultValue = "empty") String challenge) {
-        System.out.println("********************************************************************");
-        System.out.println(mode);
-        System.out.println(token);
-        System.out.println(challenge);
-        System.out.println("********************************************************************");
+        logger.info("********************************************************************");
+        logger.info(mode);
+        logger.info(token);
+        logger.info(challenge);
+        logger.info("********************************************************************");
         return challenge;
     }
 }
